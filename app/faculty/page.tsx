124b812be9c6ca1e616aa7ae5f40d128a1e4b3ee
Babu <babu@gmail.com>
2024-08-21 19:29:27 +0530

    intial commit

---
+++ app/faculty/page.tsx
@@ -0,0 +1,5 @@
+export default function FacultyPage(){
+    return <div className="min-h-screen w-full bg-red-100">
+        
+    </div>
+}
---
+++ app/student/page.tsx
@@ -0,0 +1,5 @@
+export default function StudentPage(){
+    return <div className="min-h-screen w-full bg-black">
+
+    </div>
+}
